5e0937d3352a600c473b3236c64c6aa7ec0ccfff
snely <dev.snely@gmail.com>
2025-05-15 21:16:44 +0000

    On peut utiliser ctrl+alt+h pour rouler le projet avec le script.

--- appbase/src/main/java/com/example/App.java
+++ appbase/src/main/java/com/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        // Pour rouler le projet, Vous pouvez taper la commande suivante:
+        // ctrl + alt + h
+        // Cela va compiler le projet et l'exécuter
         System.out.println("Hello World!" );
     }
 }
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp appbase/target/classes com.example.App
